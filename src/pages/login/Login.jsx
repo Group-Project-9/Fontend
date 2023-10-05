cf55a6a85875b604e8c9c160ebd6a49db2b71eb3
PhetployST <139663878+PhetployST@users.noreply.github.com>
2023-10-06 01:19:10 +0700

    login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -7,55 +7,84 @@ import LogoMB from '../../assets/icon-mb.png';
 
 const Login = () => {
 
-  const [logo, setLogo] = useState(LogoDT);
+  const [action,setAction] = useState("Login");
 
-  const handleLogoChange = () => {
-    const windowWidth = window.innerWidth;
-    if (windowWidth <= 1279) {
-        setLogo(LogoMB);
-    } else {
-        setLogo(LogoDT);
-    }
-};
+  // const [logo, setLogo] = useState(LogoDT);
 
-useEffect(() => {
-    handleLogoChange(); 
-    window.addEventListener("resize", handleLogoChange);
-    return () => {
-        window.removeEventListener("resize", handleLogoChange);
-    };
-}, []);
+//   const handleLogoChange = () => {
+//     const windowWidth = window.innerWidth;
+//     if (windowWidth <= 1279) {
+//         setLogo(LogoMB);
+//     } else {
+//         setLogo(LogoDT);
+//     }
+// };
+
+// useEffect(() => {
+//     handleLogoChange(); 
+//     window.addEventListener("resize", handleLogoChange);
+//     return () => {
+//         window.removeEventListener("resize", handleLogoChange);
+//     };
+// }, []);
   
   return (
-  <div 
-    className='bg-white flex items-center justify-center'
-    id='login-main'
+  <div id='login-main' 
+  // className='flex items-center justify-center' 
   >
-    <div className='w-[50%] flex text-center justify-center'>
+   
+   {/* div logo */}
+  {/* <div 
+    className='w-[50%] flex text-center justify-center'>
       <img src={logo} alt="logo-webpage" />
-    </div>
-    <div className='w-[50%] flex flex-col items-center justify-center'>
-      <div>
-        <form className='flex flex-col container'>
-          <div className='border-2 border-black p-5 flex flex-col'>
-            <h1 className='text-black text-xl font-bold'>LOGIN</h1>
-            <label htmlFor="username">
-              <input type="username" id="username" value="" placeholder="Enter your email ... " />
-            </label>
-            <label htmlFor="">
-              <input type="password" id="password" value="" placeholder="Enter your password ... " />
-            </label>
+  </div> */}
+
+    {/* div for sing up */}
+    <div>
+      <div className='container'>
+
+        <div className='header'>
+          <div className='text'>{action}</div>
+          <div className='underline'></div>
+          <div className='small-text'>Please enter your username and password!</div>
+        </div>
+
+        <div className='inputs'>
+
+          <div className='input'>
+            <img src='' alt=''/>
+            <input type='text' placeholder='Username'/>
           </div>
-        </form>
+
+          <div className='input'>
+            <img src='' alt=''/>
+            <input type='email' placeholder='Email'/>
+          </div>
+
+          <div className='input'>
+            <img src='{}' alt=''/>
+            <input type='Password' placeholder='Password'/>
+          </div>
+
+      <div className='forgot-password'><span>Forgot your password?</span></div>
+
+      <div className='submit-container'>
+        <div className='submit'><button>Login</button></div>
       </div>
-      <div className='w-[50%]'>
-        <a href="#">Forgot your password?</a>
-        <button>Sign In</button>
-        <span>Don’t have an account? <a href="#">Sign up</a></span>
-      </div>
+
+
+        </div>
     </div>
-    
-{/* 
+
+
+
+        {/* <div className='w-[50%]'>
+          <a href="#">Forgot your password?</a>
+          <button>Sign In</button>
+          <span>Don’t have an account? <a href="#">Sign up</a></span>
+        </div> */}
+
+        {/* 
       <div id='temporary-link'>
         <h4>temporary link for checking</h4>
         <ul>
@@ -77,7 +106,11 @@ useEffect(() => {
         </ul>
       </div> */}
     </div>
+    
+
+    </div>
   )
 }
+
 
 export default Login
