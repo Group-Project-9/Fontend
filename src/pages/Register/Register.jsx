7c62dc0b6d822af507af87b7af5e233a9bd0708a
Sonsawan Ngamsom <86834525+murphy6867@users.noreply.github.com>
2023-10-18 11:29:49 +0700

    Update validation form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -6,6 +6,8 @@ const Register = () => {
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError]  = useState(null);
+  const [matchPassword, setMatchPassword] = useState(true);
+  const [selectGender, setSelectGender] = useState(true);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -21,9 +23,16 @@ const Register = () => {
 
     const { password, confirmPassword } = formData;
     if (password !== confirmPassword) {
-      alert("Passwords do not match");
+      setMatchPassword(false)
       return;
     }
+    if (!Object.prototype.hasOwnProperty.call(formData, 'gender')) {
+      setSelectGender(false)
+      return;
+    }
+    setMatchPassword(true)
+    setSelectGender(true)
+    console.log(formData);
 
     try {
       setLoading(true);
@@ -34,7 +43,6 @@ const Register = () => {
         },
         body: JSON.stringify(formData),
       });
-
       const data = await request.json();
       if (data.success === false) {
         setLoading(false);
@@ -75,13 +83,13 @@ const Register = () => {
                 type="text" 
                 placeholder="First name ..." 
                 className="w-1/2 border-2 border-gray-200 p-3 rounded-lg bg-white" 
-                id="fristName" 
+                id="firstname" 
                 onChange={handleChange} />
               <input 
                 type="text" 
                 placeholder="Last name ..." 
                 className="w-1/2 border-2 border-gray-200 p-3 rounded-lg bg-white" 
-                id="lastName" 
+                id="lastname" 
                 onChange={handleChange} />
             </div>
             <div className="w-full flex gap-3">
@@ -96,7 +104,7 @@ const Register = () => {
                   id="gender"
                   onChange={handleChange}
                 >
-                  <option disabled={true} value="">Select Gender</option>
+                  <option value="">Select Gender</option>
                   <option value="male">Male</option>
                   <option value="female">Female</option>
                   <option value="other">Other</option>
@@ -140,13 +148,15 @@ const Register = () => {
               {loading ? "Loading..." : "Sign Up"}
             </button>
           </form>
+          {matchPassword === false && <p className='text-red-500 mt-5'>Password does not match</p>}
+          {selectGender === false && <p className='text-red-500 mt-5'>Please select gender</p>}
           <div className='flex gap-2 mt-5'>
             <p className='text-md  text-black'>Have an account?</p>
             <Link to={'/login'}>
               <span className='text-md font-bold text-blue-700'>Sign In</span>
             </Link>
           </div>
-          <div className='w-[80%] md:w-4/5 xl:w-3/5 2xl:w-full mt-5 flex justify-center items-center  hidden md:flex'>
+          <div className='w-[80%] md:w-full xl:w-full 2xl:w-full mt-5 flex justify-center items-center  hidden md:flex'>
               <div className='h-[2px] w-[20%] md:w-[10vw] xl:w-[8.5vw] 2xl:w-1/3 bg-white'></div>
               <p className='text-sm xl:text-md font-medium xl:font-bold mx-4'>Or countinue with</p>
               <div className='h-[2px] w-[20%] md:w-[10vw] xl:w-[8.5vw] 2xl:w-1/3 bg-white'></div>
